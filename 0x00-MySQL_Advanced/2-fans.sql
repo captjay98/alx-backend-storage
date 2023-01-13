d44fc657986f03a960e9159d03a89710835ce492
CaptjayZorin <captjay98@gmail.com>
2023-01-13 21:42:05 +0100

    Task 2 fixx

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,12 +1,7 @@
 -- SQL script that ranks country origins of bands
 -- ordered by the number of (non-unique) fans
 
-SELECT
-	origin, SUM(fans) AS nbfans
-FROM
-	metal_bands
-GROUP BY
-	origin
-ORDER BY
-	nbfans
-DESC
+SELECT origin, SUM(fans) AS nbfans 
+FROM metal_bands
+GROUP BY origin
+ORDER BY nbfans DESC;
